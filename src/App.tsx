0ec38e5c9bc3bc3d0a9ece8f7b22ece749eaff50
Aharon <oy.zf@qq.com>
2025-06-09 15:07:58 +0800

    style: improve viewport settings in index.html and enhance layout responsiveness in App.css by adjusting dimensions and removing unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 
 // 导入所有SVG资源
 import spinWheelBaseImg from "./assets/spin_wheel_base.svg";
-import spinPlateBaseImg from "./assets/spin_plate_base.svg";
+import spinPlateBaseImg from "./assets/spin_wheel_img.png";
 import spinPlateImg from "./assets/spin_plate.svg";
 import spinPointerImg from "./assets/spin_pointer.svg";
-import luckyWheelImg from "./assets/lucky_wheel.svg";
+import luckyWheelImg from "./assets/lucky_wheel.png";
 import logoImg from "./assets/logo.svg";
 import star1Img from "./assets/star_1.svg";
 import star2Img from "./assets/star_2.svg";
@@ -109,63 +109,52 @@ function App() {
           </div>
         </div>
         <div className="main-right-section">
-          {/* 转盘主体区域 (spin_wheel group) */}
-          <div className="spin-wheel-group">
-            {/* spin_plate_base - 转盘底座 */}
-            <div className="spin-plate-base">
-              <img src={spinPlateBaseImg} alt="Spin Plate Base" className="spin-plate-base-img" />
-            </div>
-
-            {/* spin_plate - 可旋转的转盘 */}
-            <div
-              ref={wheelRef}
-              className={`spin-plate ${isSpinning ? "spinning" : ""}`}
-              style={
-                {
-                  transform: `rotate(${rotation}deg)`,
-                  "--rotation": `${rotation}deg`,
-                } as React.CSSProperties & { "--rotation": string }
-              }>
-              <img src={spinPlateImg} alt="Spin Plate" className="spin-plate-img" />
-
-              {/* 转盘文字 - BigWin - 保持竖直 */}
-              <div className="prize-text bigwin-text" style={{ transform: `rotate(${-rotation}deg)` }}>
-                Big
-                <br />
-                Win
+          <div className="spin-wheel-container">
+            {/* 转盘主体区域 (spin_wheel group) */}
+            <div className="spin-wheel-group">
+              {/* spin_plate_base - 转盘底座 */}
+              <div className="spin-plate-base">
+                <img src={spinPlateBaseImg} alt="Spin Plate Base" className="spin-plate-base-img" />
               </div>
 
-              {/* 转盘文字 - Thank You - 保持竖直 */}
-              <div className="prize-text thankyou-text" style={{ transform: `rotate(${-rotation}deg)` }}>
-                Thank
-                <br />
-                You
+              {/* spin_plate - 可旋转的转盘 */}
+              <div
+                ref={wheelRef}
+                className={`spin-plate ${isSpinning ? "spinning" : ""}`}
+                style={
+                  {
+                    transform: `rotate(${rotation}deg)`,
+                    "--rotation": `${rotation}deg`,
+                  } as React.CSSProperties & { "--rotation": string }
+                }>
+                <img src={spinPlateImg} alt="Spin Plate" className="spin-plate-img" />
+
+                {/* 转盘文字 - BigWin - 保持竖直 */}
+                <div className="prize-text bigwin-text" style={{ transform: `rotate(${-rotation}deg)` }}>
+                  Big
+                  <br />
+                  Win
+                </div>
+
+                {/* 转盘文字 - Thank You - 保持竖直 */}
+                <div className="prize-text thankyou-text" style={{ transform: `rotate(${-rotation}deg)` }}>
+                  Thank
+                  <br />
+                  You
+                </div>
+              </div>
+
+              {/* spin_pointer - 中心指针按钮 */}
+              <div className="spin-pointer-group">
+                <button
+                  className={`spin-pointer-button ${isSpinning ? "spinning" : ""}`}
+                  onClick={spinWheel}
+                  disabled={isSpinning}>
+                  <img src={spinPointerImg} alt="Spin Pointer" className="spin-pointer-img" />
+                </button>
               </div>
             </div>
 
-            {/* spin_pointer - 中心指针按钮 */}
-            <div className="spin-pointer-group">
-              <button
-                className={`spin-pointer-button ${isSpinning ? "spinning" : ""}`}
-                onClick={spinWheel}
-                disabled={isSpinning}>
-                <img src={spinPointerImg} alt="Spin Pointer" className="spin-pointer-img" />
-              </button>
-            </div>
-          </div>
-
-          {/* spin_wheel_base - 底部装饰条 */}
-          <div className="spin-wheel-base">
-            {/* 金币堆装饰 */}
-            <div className="coin-staf-1">
-              <img src={coinStaf1Img} alt="Coin Staff 1" />
-            </div>
-            <div className="coin-staf-2">
-              <img src={coinStaf2Img} alt="Coin Staff 2" />
-            </div>
-            <div className="spin-wheel-base-img-container">
-              <img src={spinWheelBaseImg} alt="Spin Wheel Base" className="spin-wheel-base-img" />
-            </div>
           </div>
         </div>
 
@@ -199,35 +188,32 @@ function App() {
 
         {/* Congratulations 弹窗 */}
         {showCongratulations && (
-          <div className="congratulations-overlay">
-            {/* 黑色半透明遮罩 */}
-            <div className="congratulations-mask"></div>
+          <div className="thank-you-overlay">
+          {/* 黑色半透明遮罩 */}
+          <div className="thank-you-mask"></div>
 
-            {/* 弹窗主体 */}
-            <div className="congratulations-modal">
-              {/* 标题区域 - 整个使用title.svg */}
-              <img src={titleImg} alt="Title" className="modal-title-img" onClick={closeCongratulations} />
+          {/* 弹窗主体 */}
+          <div className="thank-you-modal">
+            {/* 标题区域 - 整个使用title.svg */}
+            <img src={titleImg} alt="Title" className="modal-title-img" onClick={closeCongratulations} />
 
-              {/* 主要内容区域 */}
-              <div className="modal-main-content">
-                <h2 className="congratulations-title">Congratulations!</h2>
-                <p className="congratulations-subtitle">
-                  Please show the screen to our staff
-                  <br />
-                  before tapping "OK"
-                </p>
-                <div className="modal-button-group" onClick={closeCongratulations}>
-                  <div className="modal-button-base5"></div>
-                  <div className="modal-button-base8"></div>
-                  <div className="modal-button-base9"></div>
-                  <div className="modal-button-text">OK</div>
-                </div>
+            {/* Thank You主要内容区域 - 独立样式 */}
+            <div className="thank-you-main-content">
+              <h2 className="thank-you-main-title">Congratulations!</h2>
+              <p className="thank-you-subtitle">Please show the screen to our staff</p>
+
+              <p className="thank-you-subtitle">before tapping "OK"</p>
+
+              <div className="modal-button-group" onClick={closeCongratulations}>
+                <div className="modal-button-base5"></div>
+                <div className="modal-button-base8"></div>
+                <div className="modal-button-base9"></div>
+                <div className="modal-button-text">OK</div>
               </div>
-
-              {/* OK按钮 - 整个使用button.svg */}
-              {/* <img src={buttonImg} alt="OK Button" className="modal-button-img" onClick={closeCongratulations} /> */}
             </div>
           </div>
+        </div>
+
         )}
 
         {/* Thank You 弹窗 - title和button复用congratulations样式 */}
